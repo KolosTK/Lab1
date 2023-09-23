4a6968a78779d0ec64a45a613ee7b5f8068025f5
KolosTK <tanya.kolosenko.1705@gmail.com>
2023-09-23 12:39:54 +0300

    Create list of books

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //Book [] bookCollection=new Book;
+        List<Book> bookCollection = new ArrayList<Book>();
+        Book bookExample = new Book ();
+        bookCollection.add(bookExample);
 
-        Book first = new Book ();
-        first.AddNewBook("451 Pharenheit", "Rey Bredbery", "12909-15134-612-2", 1853);
-        first.ShowAllBooks();
+        bookExample.AddNewBook("451 Pharenheit", "Rey Bredbery", "12909-15134-612-2", 1853);
+        bookExample.ShowAllBooks();
     }
 }
