545edf98f4b5f61a01cdcc8f3b2b1a0ca9858988
KolosTK <tanya.kolosenko.1705@gmail.com>
2023-09-23 22:25:18 +0300

    Create DeleteByISBN method in main file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,6 +29,25 @@ public class Main {
         {
             System.out.println("This book is not exist");
         }
+
+
+    }
+
+    static public void DeleteByISBN(String deletingISBN, List<Book> bookCollection) {
+        Book GlobalBook = new Book();
+        boolean result = false;
+        for (int i = 0; i < bookCollection.size(); i++) {
+            GlobalBook = bookCollection.get(i);
+            if (deletingISBN==GlobalBook.ISBN){
+                result = true;
+                bookCollection.remove(GlobalBook);
+                break;
+            }
+        }
+        if(result == false)
+        {
+            System.out.println("Incorrect ISBN number");
+        }
     }
 
 
@@ -44,13 +63,27 @@ public class Main {
         bookCollection.add(second);
         second.AddNewBook("Nineteen Eighty-Four", "George Orwell", "444-9-78-554884-0", 1949);
 
+        Book third = new Book();
+        bookCollection.add(third);
+        third.AddNewBook("A Little Life", "Hanya Yanagihara", "588-8-10-584770-8", 2015);
+
         //Second task
+
         ShowAllBooks(bookCollection);
 
         //Third task
-        String nameOfSearchingBook = "The Gre at Gatsby";
-        SearchByName(nameOfSearchingBook, bookCollection);
+        System.out.println("\nSearching book is :");
 
-        //Fourth rask
+        String nameOfSearchingBook1 = "The Great Gatsby";
+        SearchByName(nameOfSearchingBook1, bookCollection);
+        System.out.println("\nSecond try :\n");
+        String nameOfSearchingBook2 = "The Gre  at Gatsby";
+        SearchByName(nameOfSearchingBook2, bookCollection);
+        System.out.println("\n");
+        //Fourth task
+        String searchingISDN = "156-5-48-785433-8";
+        DeleteByISBN(searchingISDN, bookCollection);
+
+        ShowAllBooks(bookCollection);
     }
 }
