960d05d0023a04e677eabed6e3d34a2d0818be22
KolosTK <tanya.kolosenko.1705@gmail.com>
2023-09-23 21:48:29 +0300

    Create SearchByName method in main file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class Main {
     static public void ShowAllBooks(List<Book> bookCollection) {
         Book GlobalBook = new Book();
@@ -15,20 +14,43 @@ public class Main {
         }
     }
 
+    static public void SearchByName(String nameOfBook, List<Book> bookCollection) {
+        Book GlobalBook = new Book();
+
+        boolean result = false;
+        for (int i = 0; i < bookCollection.size(); i++) {
+            GlobalBook = bookCollection.get(i);
+            if (nameOfBook==GlobalBook.name){
+                result = true;
+                System.out.println("Name of book : " + GlobalBook.name + "\tAuthor : " + GlobalBook.author + "\tISBN : " + GlobalBook.ISBN + "\t Year : " + GlobalBook.yearOfPublishing);
+            }
+        }
+        if (result==false)
+        {
+            System.out.println("This book is not exist");
+        }
+    }
+
 
     public static void main(String[] args) {
-        //Book [] bookCollection=new Book;
+        //First task
         List<Book> bookCollection = new ArrayList<Book>();
-        Book first = new Book ();
+        Book first = new Book();
         bookCollection.add(first);
 
         first.AddNewBook("The Great Gatsby", "Scott Fitzgerald", "156-5-48-785433-8", 1925);
 
-        Book second = new Book ();
+        Book second = new Book();
         bookCollection.add(second);
         second.AddNewBook("Nineteen Eighty-Four", "George Orwell", "444-9-78-554884-0", 1949);
+
+        //Second task
         ShowAllBooks(bookCollection);
 
+        //Third task
+        String nameOfSearchingBook = "The Gre at Gatsby";
+        SearchByName(nameOfSearchingBook, bookCollection);
 
-       }
+        //Fourth rask
+    }
 }
